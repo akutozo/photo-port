9641a87c760a90940b97215979b781af07c7f114
Jacob Daughtry <akutozo@gmail.com>
2020-10-18 13:33:14 -0500

    File corrections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import Nav from './components/Nav';
+import './App.css';
 import About from './components/About';
+import Nav from './components/Nav';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
-
   const [contactSelected, setContactSelected] = useState(false);
-  
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -26,18 +26,20 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        {/* <div> */}
+        <div>
           {!contactSelected ? (
-            <>
-              <Gallery currentCategory={currentCategory}></Gallery>
-              <About></About>
-            </>
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
           ) : (
               <ContactForm></ContactForm>
             )}
-        {/* </div> */}
+        </div>
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,57 +1,57 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useState, useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-
-    const {
-        categories = [],
-        setCurrentCategory,
-        currentCategory,
-        contactSelected,
-        setContactSelected
-      } = props;
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+    contactSelected,
+    setContactSelected
+  } = props;
 
     useEffect(() => {
         document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
-
+      }, [currentCategory]);
+  
     return (
-        <header className="flex-row px-1">
+      <header className="flex-row px-1">
         <h2>
-            <a data-testid="link" href="/">
+          <a data-testid="link" href="/">
             <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-            </a>
+          </a>
         </h2>
         <nav>
-            <ul className="flex-row">
+          <ul className="flex-row">
             <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
                 About me
-                </a>
+              </a>
             </li>
             <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-                <span onClick={() => setContactSelected(true)}>Contact</span>
+              <span onClick={() => setContactSelected(true)}>Contact</span>
             </li>
             {categories.map((category) => (
-                <li
-                  className={`mx-1 ${
-                    currentCategory.name === category.name && !contactSelected && `navActive`
-                    }`}
-                  key={category.name}
-                >
-                <span onClick={() => {
+              <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && `navActive`
+                }`}
+              key={category.name}
+            >
+                <span
+                  onClick={() => {
                     setCurrentCategory(category);
                     setContactSelected(false);
-                }}
+                  }}
                 >
-                    {capitalizeFirstLetter(category.name)}
+                  {capitalizeFirstLetter(category.name)}
                 </span>
-                </li>
+              </li>
             ))}
-            </ul>
+          </ul>
         </nav>
-        </header>
+      </header>
     );
-}
+  }
 
 export default Nav;
